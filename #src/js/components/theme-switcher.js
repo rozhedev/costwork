7ed9b679b54292fd8e443773363a3ab7c12745e0
Coder <coder@gmail.com>
2022-06-05 12:30:09 +0300

    Create footer & add decoration waves for main page

--- #src/js/components/theme-switcher.js
+++ #src/js/components/theme-switcher.js
@@ -1,8 +1,8 @@
 const themeSwitcher = document.querySelector(".theme-switcher");
 const themeSwitcherIcon = document.querySelector(".theme-switcher svg use");
 
-const logo = document.querySelector(".logo img");
-const logoWebp = document.querySelector('source[srcset="img/vector_img/logo.svg"]');
+const logo = document.querySelectorAll(".logo img");
+const logoWebp = document.querySelectorAll('source[srcset="img/vector_img/logo.svg"]');
 const mainImg = document.querySelector(".main__img");
 const mainImgWebp = document.querySelector('source[srcset="../img/vector_img/main-img.svg"]');
 
@@ -13,18 +13,27 @@ if (themeSwitcher) {
 
         // * IMAGE REPLACE
         themeSwitcherIcon.setAttribute("href", "img/icons/icons.svg#sun");
-        logo.setAttribute("src", "img/vector_img/dark-logo.svg");
-        logoWebp.setAttribute("srcset", "img/vector_img/dark-logo.svg");
         mainImg.setAttribute("src", "img/vector_img/dark-main-img.svg");
         mainImgWebp.setAttribute("srcset", "../img/vector_img/dark-main-img.svg");
 
+        for (logoItem of logo) {
+            logoItem.setAttribute("src", "img/vector_img/dark-logo.svg");
+        }
+        for (logoWebpItem of logoWebp) {
+            logoWebpItem.setAttribute("srcset", "img/vector_img/dark-logo.svg");
+        }
 
         if (!themeSwitcher.classList.contains("_dark")) {
             themeSwitcherIcon.setAttribute("href", "img/icons/icons.svg#lune");
-            logo.setAttribute("src", "img/vector_img/logo.svg");
-            logoWebp.setAttribute("srcset", "img/vector_img/logo.svg");
             mainImg.setAttribute("src", "img/vector_img/main-img.svg");
             mainImgWebp.setAttribute("srcset", "../img/vector_img/main-img.svg");
+
+            for (logoItem of logo) {
+                logoItem.setAttribute("src", "img/vector_img/logo.svg");
+            }
+            for (logoWebpItem of logoWebp) {
+                logoWebpItem.setAttribute("srcset", "img/vector_img/logo.svg");
+            }
         }
     });
 }
