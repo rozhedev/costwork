ec017bdbb9eac2c29e274a5b9a4c9d4c7f6349d9
Coder <coder@gmail.com>
2022-06-05 20:38:42 +0300

    Create about page & it's adaptive versions

--- #src/js/components/theme-switcher.js
+++ #src/js/components/theme-switcher.js
@@ -5,6 +5,8 @@ const logo = document.querySelectorAll(".logo img");
 const logoWebp = document.querySelectorAll('source[srcset="img/vector_img/logo.svg"]');
 const mainImg = document.querySelector(".main__img");
 const mainImgWebp = document.querySelector('source[srcset="../img/vector_img/main-img.svg"]');
+const aboutImg = document.querySelector(".about__img");
+const aboutImgWebp = document.querySelector('source[srcset="../img/photos/light-mockup.webp"]');
 
 if (themeSwitcher) {
     themeSwitcher.addEventListener("click", function (e) {
@@ -13,26 +15,36 @@ if (themeSwitcher) {
 
         // * IMAGE REPLACE
         themeSwitcherIcon.setAttribute("href", "img/icons/icons.svg#sun");
-        mainImg.setAttribute("src", "img/vector_img/dark-main-img.svg");
-        mainImgWebp.setAttribute("srcset", "../img/vector_img/dark-main-img.svg");
-
         for (logoItem of logo) {
             logoItem.setAttribute("src", "img/vector_img/dark-logo.svg");
         }
         for (logoWebpItem of logoWebp) {
             logoWebpItem.setAttribute("srcset", "img/vector_img/dark-logo.svg");
         }
+        if (mainImg && mainImgWebp) {
+            mainImg.setAttribute("src", "../img/vector_img/dark-main-img.svg");
+            mainImgWebp.setAttribute("srcset", "../img/vector_img/dark-main-img.svg");
+        }
+        if (aboutImg && aboutImgWebp) {
+            aboutImg.setAttribute("src", "../img/photos/dark-mockup.png");
+            aboutImgWebp.setAttribute("srcset", "../img/photos/dark-mockup.webp");
 
+        }
         if (!themeSwitcher.classList.contains("_dark")) {
             themeSwitcherIcon.setAttribute("href", "img/icons/icons.svg#lune");
-            mainImg.setAttribute("src", "img/vector_img/main-img.svg");
-            mainImgWebp.setAttribute("srcset", "../img/vector_img/main-img.svg");
-
             for (logoItem of logo) {
                 logoItem.setAttribute("src", "img/vector_img/logo.svg");
             }
             for (logoWebpItem of logoWebp) {
                 logoWebpItem.setAttribute("srcset", "img/vector_img/logo.svg");
+            }
+            if (mainImg && mainImgWebp) {
+                mainImg.setAttribute("src", "../img/vector_img/main-img.svg");
+                mainImgWebp.setAttribute("srcset", "../img/vector_img/main-img.svg");
+            }
+            if (aboutImg && aboutImgWebp) {
+                aboutImg.setAttribute("src", "../img/photos/light-mockup.png");
+                aboutImgWebp.setAttribute("srcset", "../img/photos/light-mockup.webp");
             }
         }
     });
