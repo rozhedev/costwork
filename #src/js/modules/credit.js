37e4f9cdc2e052076e4676ec6b6bb37e2c723534
Coder <coder@gmail.com>
2022-06-13 11:25:52 +0300

    Create differential calculator. Move result-item styles to separate file

--- #src/js/modules/credit.js
+++ #src/js/modules/credit.js
@@ -20,7 +20,7 @@ let delay = 3000;
 // * ANNUITY CREDIT
 
 function calcAnnMonthlyPayment(amount, rate, period) {
-    let monthlyRate = rate / (100 * 12);
+    let monthlyRate = rate / 100 / 12;
     return +(amount * (monthlyRate / (1 - (1 + monthlyRate) ** -period))).toFixed(2);
 }
 
@@ -28,7 +28,7 @@ function calcAnnOverpayment(amount, period, rate) {
     return +((calcAnnMonthlyPayment(amount, period, rate) * period) - amount).toFixed(2);
 }
 
-// * OUTPUT
+// * ANNUITY OUTPUT
 
 if (annCreditAmount && annYearRate && annCreditPeriod) {
     for (inpItem of allFormInp) {
@@ -66,6 +66,7 @@ if (annCreditAmount && annYearRate && annCreditPeriod) {
     }
 }
 
+
 // * DIFFERENTIAL CREDIT
 
 const diffCreditAmount = document.getElementById("diff-credit-amount");
@@ -76,21 +77,89 @@ const diffMonthlyFee = document.getElementById("diff-monthly-fee");
 
 const diffTotalPaymentOutput = document.getElementById("diff-total-payment-output");
 const diffOverpaymentOutput = document.getElementById("diff-overpayment-output");
-const diffMessageOutput = document.getElementById("diff-message-output");
-const diffOutputTable = document.getElementById("diff-payment-table");
+const diffTableOutput = document.querySelector("#diff-table-output tbody");
 
-function calcDiffMonthlyPayment(amount, rate, period) {
+function calcDiffPayment(amount, rate, period, monthlyFee) {
     let primaryPayment = amount / period;
-    let arr = [];
+    let monthlyRate = rate / 100 / 12;
+    let monthlyPayobj = {
+        monthlyPayment: [],
+        monthlyOverpayment: [],
+    };
 
     for (i = 1; i <= period; i++) {
-        let percentPayment = (amount - 500 * i) * (rate / 100) / 365 * 30;
-        arr[i] = +(primaryPayment + percentPayment).toFixed(2);
+        let monthlyPercent = (amount - primaryPayment * i) * monthlyRate;
+        monthlyPayobj.monthlyPayment[i] = +(primaryPayment + monthlyPercent + monthlyFee).toFixed(2);
     }
-    return arr;
+    for (i = 1; i <= period; i++) {
+        let monthlyPercent = (amount - primaryPayment * i) * monthlyRate;
+        monthlyPayobj.monthlyOverpayment[i] = +(monthlyPercent + monthlyFee).toFixed(2);
+    }
+    console.log(monthlyPayobj);
+    return monthlyPayobj;
+}
+
+// * DIFF TOTAL SUMMA
+
+function calcDiffTotalSum(arr1, arr2, oneTimeFee) {
+    let paymentSumObj = {
+        monthlyPaymentSum: 0,
+        monthlyOverpaymentSum: 0,
+    }
+
+    for (let i = 1; i < arr1.length; i++) {
+        paymentSumObj.monthlyPaymentSum += arr1[i];
+        paymentSumObj.monthlyOverpaymentSum += arr2[i];
+    }
+    paymentSumObj.monthlyPaymentSum = +(paymentSumObj.monthlyPaymentSum + oneTimeFee).toFixed(2);
+    paymentSumObj.monthlyOverpaymentSum = +(paymentSumObj.monthlyOverpaymentSum + oneTimeFee).toFixed(2);
+
+    return paymentSumObj;
 }
 
 // * OUTPUT
+
+function createPaymentTable(table, arr1, arr2, totalSum1, totalSum2) {
+    for (let i = 1; i < arr1.length; i++) {
+        let tableItem = document.createElement("tr");
+        tableItem.classList.add("payment-table__item");
+
+        tableItem.innerHTML += `
+        <td class="payment-table__item-label navlink">
+            ${i} платіж:
+        </td>
+        <td class="payment-table__item-value">
+            <span class="navlink">${arr1[i]}</span>
+            <span class="navlink currency">UAH</span>
+        </td>
+        <td class="payment-table__item-value">
+            <span class="navlink">${arr2[i]}</span>
+            <span class="navlink currency">UAH</span>
+        </td>
+              `;
+
+        table.appendChild(tableItem);
+    }
+
+    let tableItemSum = document.createElement("tr");
+    tableItemSum.classList.add("payment-table__item");
+    tableItemSum.innerHTML += `
+        <td class="payment-table__item-label navlink">
+            Сумма:
+        </td>
+        <td class="payment-table__item-value">
+            <span class="navlink">${totalSum1}</span>
+            <span class="navlink currency">UAH</span>
+        </td>
+        <td class="payment-table__item-value">
+            <span class="navlink">${totalSum2}</span>
+            <span class="navlink currency">UAH</span>
+        </td>
+              `;
+    table.appendChild(tableItemSum);
+}
+
+// * DIFFERENTIAL EVENT
 
 if (diffCreditAmount && diffYearRate && diffCreditPeriod) {
     for (inpItem of allFormInp) {
@@ -104,45 +173,13 @@ if (diffCreditAmount && diffYearRate && diffCreditPeriod) {
                 let diffMonthlyFeeValue = +document.querySelector("#diff-monthly-fee").value;
 
                 setTimeout(function () {
-                    let diffMonthlyPayment = calcDiffMonthlyPayment(diffCreditAmountValue, diffCreditPeriodValue, diffYearRateValue) + diffMonthlyFeeValue;
-                    annMonthlyPaymentOutput.textContent = diffMonthlyPayment;
+                    let paymentObj = calcDiffPayment(diffCreditAmountValue, diffYearRateValue, diffCreditPeriodValue, diffMonthlyFeeValue);
+                    let totalSumObj = calcDiffTotalSum(paymentObj.monthlyPayment, paymentObj.monthlyOverpayment, diffOneTimeFeeValue);
+                    diffTableOutput.innerHTML = "";
 
-                    let diffTotalPayment = (diffMonthlyPayment * diffCreditPeriodValue + diffOneTimeFeeValue).toFixed(2);
-                    diffTotalPaymentOutput.textContent = diffTotalPayment;
-
-                    let diffOverpayment = calcAnnOverpayment(diffCreditAmountValue, diffCreditPeriodValue, diffYearRateValue);
-                    if (diffOverpayment > 0) {
-                        diffOverpaymentOutput.textContent = Math.abs(diffOverpayment);
-                    } else {
-                        diffOverpaymentOutput.textContent = diffOverpayment;
-                        diffOverpaymentOutput.style.color = errorRedColor;
-                        diffMessageOutput.textContent = "Умови кредиту невигідні для банку, так як позичальник віддає менше чим позичає. Таку пропозицію неможливо знайти.";
-                    }
+                    createPaymentTable(diffTableOutput, paymentObj.monthlyPayment, paymentObj.monthlyOverpayment, totalSumObj.monthlyPaymentSum, totalSumObj.monthlyOverpaymentSum);
                 }, delay);
             }
         });
     }
 }
-
-// function getTableRate() {
-//     for (let i = 0; i < currenciesInfo.length; i++) {
-//         // * CREATE TABLE
-
-//         let tableItem = document.createElement("tr");
-//         tableItem.classList.add("payment-table__item");
-
-//         tableItem.innerHTML += `<td class="payment-table__item-content">
-//                   <h3 class="payment-table__item-title">${currenciesInfo[i].titleName}</h3>
-//               </td>
-//               <td class="payment-table__item-course"></td>
-//               `;
-
-//         cryptoRateTable.appendChild(tableItem);
-//     }
-// }
-
-// let temp = calcDiffMonthlyPayment(creditAmount, yearRate, creditPeriod);
-
-// for (let i = 1; i < temp.length; i++) {
-//     console.log(`${i} платіж - ${temp[i]}`);
-// }
